97b91fffb40022852d7b4396bff1fcd93e90dc70
aniboe <140517030+aniboe@users.noreply.github.com>
2026-07-07 22:33:07 +0530

    add dotenv, mongoose database connection, and server setup

---
+++ backend/src/DB/mongoConnect.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+
+export const dbConnect = async() => {
+    try {
+        return (await mongoose.connect(`${process.env.DB_CONNECT}${process.env.DB_NAME}`))
+    } catch (error) {
+        console.log("DB connection failed", error)
+        console.log("server will shut down since to database found")
+        process.exit(1) // exits ans quites wole server
+    }
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,33 @@
-import express from "express"
+import dotenv from 'dotenv'
+dotenv.config({path: ".env"})
 
-const app = express()
 
-app.get("/",(req , res ) => {
-    res.json("this is a test")
-})
+import {app} from "./server.js"
+import {dbConnect} from "./DB/mongoConnect.js"
+
+
+const port = process.env.PORT || 3000
+console.log(port);
+
+
+
+// call the function to execute when server iniates
+dbConnect()
+    .then((value) => {
+        // console.log(value) // lots of data
+        app.get("/",(req , res ) => {
+            res.json(`server is runing on http://localhost:${port}/`)
+        })
+
+        app.listen(port,() => {
+            console.log(`server is runing on http://localhost:${port}/`);
+            
+        })
+    })
+    .catch((err) => {
+        console.log( err || "something went wrong while connecting to database")
+    })
+
+
 
 app.listen(3000)
