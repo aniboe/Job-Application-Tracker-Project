f920c689e455f1b3f3d9749d1fbc9cdcbf89b278
aniboe <140517030+aniboe@users.noreply.github.com>
2026-09-25 19:34:04 +0530

    email connection error fix attempt 2

--- backend/src/utility/NodeMailer.js
+++ backend/src/utility/NodeMailer.js
@@ -1,12 +1,26 @@
-import nodemailer from "nodemailer"
+// import nodemailer from "nodemailer"
 
 
+
+// export const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   secure: true,
+//   auth: {
+//     user: process.env.GMAIL_USER,
+//     pass: process.env.GOOGLE_APP_PASSWORD,
+//   },
+// });
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
-  secure: true,
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GOOGLE_APP_PASSWORD,
   },
+  connectionTimeout: 20000,
+  greetingTimeout: 20000,
+  socketTimeout: 20000,
 });
